878c334353d734e1a002cfadf129ec01dc5cc396
Fabián Lozano Ballesteros <fabianlozano044@gmail.com>
2021-10-10 13:45:56 -0500

    test de flask

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, request, make_response, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello world Flask'
+    user_ip = request.remote_addr
+
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip', user_ip)
+    
+    return response
+
+@app.route('/hello')
+def hello():
+    user_ip = request.cookies.get('user_ip')
+    return 'Hello world, tu IP es {}'.format(user_ip)
+
+app.run(debug=True)
