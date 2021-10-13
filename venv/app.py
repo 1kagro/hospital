46b85d8682c69e93b2d8f7b2b0e4d2de84f8682b
Fabián Lozano Ballesteros <fabianlozano044@gmail.com>
2021-10-12 20:44:17 -0500

    flask

---
+++ venv/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, url_for, request
+
+app = Flask(__name__, template_folder='../templates', static_folder='../static')
+
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+@app.route('/register', methods=['POST'])
+def register():
+    return render_template('register.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
