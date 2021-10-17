cf66071438e76e095ac2f880cf57a0ddf306b945
ipedrosa12.Italo <ipedrosa12.ipedrosa12@gmail.com>
2021-10-17 13:02:43 -0500

    Se utilizó display grid en dash1

--- app.py
+++ app.py
@@ -2,10 +2,6 @@ from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
-
-
-
-
 @app.route('/', methods=['GET', 'POST'])
 def home():
     return render_template('index.html')
